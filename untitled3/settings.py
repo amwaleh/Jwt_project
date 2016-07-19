e4e427159b20ac94d8073042ebadad454e601ea7
alex mwaleh <alex.mwaleh@andela.com>
2016-07-19 11:39:42 +0300

    add added settingd for setting.py heroku config

--- untitled3/settings.py
+++ untitled3/settings.py
@@ -154,12 +154,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
-STATICFILES_DIRS = (os.path.join(os.path.join(BASE_DIR, 'static')))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(os.path.join(BASE_DIR, 'static')),)
 # redirect after login with allauth
 LOGIN_REDIRECT_URL = "/"
 # comment the next line to send email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SITE_ID = 1
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
