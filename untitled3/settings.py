d8ad9da8ad4b7a1b9c338e1a29ab3287aedfbbbb
alex mwaleh <alex.mwaleh@andela.com>
2016-07-19 11:30:41 +0300

    add procfile

--- untitled3/settings.py
+++ untitled3/settings.py
@@ -153,10 +153,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+
+
+STATICFILES_DIRS = (os.path.join(os.path.join(BASE_DIR, 'static')))
 # redirect after login with allauth
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
 # comment the next line to send email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 SITE_ID = 1
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
